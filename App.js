75c54fad1780d490a25e8766ca83805195617363
Ryan Kubik <rmkubik@mix.wvu.edu>
2018-03-05 23:32:21 -0800

    Move brain out of App, create render action

--- App.js
+++ App.js
@@ -3,19 +3,16 @@ import { h, app } from 'hyperapp';
 import './src/css/App.scss';
 
 import Game from './src/game/Game';
-import PlayerBrain from './src/game/PlayerBrain';
 import Tile from './src/components/Tile';
 
 const game = new Game();
-const playerBrain = new PlayerBrain();
 
 const state = {
   tiles: game.grid.tiles
 }
 
 const actions = {
-  down: value => state => ({ count: state.count - value }),
-  up: value => state => ({ count: state.count + value })
+  render: value => state => ({ tiles: game.grid.tiles })
 }
 
 const view = (state, actions) => (
