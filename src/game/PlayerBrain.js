e85561f25ad6408d7b99b0ebb8ad57cfcf5099e1
Ryan Kubik <rmkubik@mix.wvu.edu>
2018-03-05 23:33:43 -0800

    Map playerbrain to input

--- src/game/PlayerBrain.js
+++ src/game/PlayerBrain.js
@@ -1,15 +1,32 @@
 import Input from './Input';
 
-class PlayerBrain {
+const playerKeys = {
+    up: 'w',
+    down: 's',
+    left: 'a',
+    right: 'd'
+}
 
-    constructor() {
-        const input = new Input(document);
-        input.listen('w', () => {console.log('w')});
-        input.listen('a', () => {console.log('a')});
-        input.listen('s', () => {console.log('s')});
-        input.listen('d', () => {console.log('d')});
+// entity needs moveComponent
+const PlayerBrain = (entity, actions) => {
+    const {move} = entity.components;
+    const input = new Input(document);
+    input.listen(playerKeys.up, () => { move.up(); actions.render(); });
+    input.listen(playerKeys.down, () => { move.down(); actions.render(); });
+    input.listen(playerKeys.left, () => { move.left(); actions.render(); });
+    input.listen(playerKeys.right, () => { move.right(); actions.render(); });
+
+    const brain = () => {
+
     }
-
+    // add input listeners to entity
+    // corresponding listeners should call corresponding moves
+    Object.assign(
+        entity.components,
+        {
+            brain
+        }
+    )
 }
 
 export default PlayerBrain;
