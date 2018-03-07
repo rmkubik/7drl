70659e5832c7b7ac147cb64c89d62f4f3d55a48d
Ryan Kubik <rmkubik@mix.wvu.edu>
2018-03-06 19:41:03 -0800

    Game render and update changes

--- src/game/Game.js
+++ src/game/Game.js
@@ -5,21 +5,44 @@ import PlayerBrain from './PlayerBrain';
 
 class Game {
 
+    entities = [];
+    actions = {};
+
     constructor() {
         const w = 9;
         const h = 9;
         this.grid = new Grid(w, h);
-        this.grid.addEntity(new Entity('👩🏻‍🎨'), {x: 4, y: 4});
+        this.addEntity(new Entity('👩🏻‍🎨'), {x: 4, y: 4});
         MoveComponent(this.grid.tiles[4][4].entity, this.grid);
+        PlayerBrain(this.grid.tiles[4][4].entity, this.render);
 
-        this.grid.addEntity(new Entity('🐞'), {x: 3, y: 2});
-        this.grid.addEntity(new Entity('🐞'), {x: 1, y: 7});
-        this.grid.addEntity(new Entity('🐞'), {x: 4, y: 8});
-        this.grid.addEntity(new Entity('🐞'), {x: 2, y: 2});
+        this.addEntity(new Entity('🐞'), {x: 3, y: 2});
+        this.addEntity(new Entity('🐞'), {x: 1, y: 7});
+        this.addEntity(new Entity('🐞'), {x: 4, y: 8});
+        this.addEntity(new Entity('🐞'), {x: 2, y: 2});
     }
 
-    bindActions(actions) {
-        PlayerBrain(this.grid.tiles[4][4].entity, actions);
+    update() {
+        // update game state every time player makes an action
+        // update all game entities, brain assigns update action?
+        this.entities.forEach((entity) => {
+            entity.update();
+            render();
+        });
+        // ^^^ turn this into a priority queue based on initiative?
+    }
+
+    render = () => {
+        this.actions.render(this.grid.tiles);
+    }
+
+    bindActions = (actions) => {
+        this.actions = actions;
+    }
+
+    addEntity(entity, position) {
+        this.entities.push(entity);
+        this.grid.addEntity(entity, position);
     }
 }
 
