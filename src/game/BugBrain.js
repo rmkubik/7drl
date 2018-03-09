4551353c03ff10d0c4ee8681b8521d58af4f5cb4
Ryan Kubik <rmkubik@mix.wvu.edu>
2018-03-08 22:47:24 -0800

    Remove BugBrain comments and add bug type

--- src/game/BugBrain.js
+++ src/game/BugBrain.js
@@ -2,12 +2,9 @@ const BugBrain = (entity, rand) => {
     const { move } = entity.components;
     if (!move) console.error("Entity does not have move component!");
 
-    // on update:
-    // randomly choose direction
-    // move in random direction
     const update = () => {
         const direction = rand.intBetween(0, 3);
-        console.log(direction);
+
         switch (direction) {
             case 0:
                 move.up();
@@ -25,6 +22,7 @@ const BugBrain = (entity, rand) => {
     }
 
     const brain = {
+        type: 'bug',
         update
     }
 
